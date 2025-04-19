8ede4ec83128df35c9a470561b6bce66a1cfbbab
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2025-04-19 05:55:01 -0300

    make a initial example

--- UseExample/Program.cs
+++ UseExample/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+using TheGarden;
+
+var garden = new Garden();
+garden.Add("plant", Color.LightGreen, 80);
+garden.Run();
+
+public class Plant
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Energy { get; set; } = 20;
+
+    public string OnFrame(
+        string onLeft, string onRight,
+        string onTop, string onBot
+    )
+    {
+        if (Random.Shared.Next(4) < 2)
+            Energy += 10;
+        
+        if (Energy > 90)
+        {
+            Energy -= 50;
+            return "REPRODUCE";
+        }
+
+        return "WAIT";
+    }
+}
